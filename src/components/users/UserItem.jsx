8a2b61dc842ab5b48600f5e74d6ee03cb19b7d0e
hernangonzalez1 <hernangonzalez1@outlook.com>
2022-08-05 22:45:55 -0400

    Added div and css classes

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -4,9 +4,11 @@ function UserItem({ user: { login, avatar_url } }) {
   return (
     <div className="card shadow-md compact side bg-base-100">
       <div className="flex-row items-center space-x-4 card-body">
-        <div className="avatar">
-          <div className="rounded-full shadow w-14 h-14">
-            <img src={avatar_url} alt="profile" />
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="profile" />
+            </div>
           </div>
         </div>
         <div>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -53,6 +53,7 @@ function User() {
             Back To Search
           </Link>
         </div>
+
         <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
           <div className="custom-card-image mb-6 md:mb-0">
             <div className="rounded-lg shadow-xl card image-full">
@@ -61,10 +62,11 @@ function User() {
               </figure>
               <div className="card-body justify-end">
                 <h2 className="card-title mb-0">{name}</h2>
-                <p>{login}</p>
+                <p className="flex-grow-0">{login}</p>
               </div>
             </div>
           </div>
+
           <div className="col-span-2">
             <div className="mb-6">
               <h1 className="text-3xl card-title">
